c0eb7a1afac27f73f04d190de3c4e1d3e65b0b5f
Pranav Kumar <pk2051872@gmail.com>
2026-08-04 08:50:31 +0530

    refactor: centralize configuration using BaseSettings

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import create_engine
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///../animedex.db"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
